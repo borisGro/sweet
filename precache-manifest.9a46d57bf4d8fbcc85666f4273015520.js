94d2ecaa521978208a19eb5f8700791a57eff6b8
theborisgros <theborisgros@penguin>
2019-05-06 20:13:35 -0700

    Updates

---
+++ precache-manifest.9a46d57bf4d8fbcc85666f4273015520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff8350c303be0296a9fb",
+    "url": "/sweet/static/css/main.0f7db3e3.chunk.css"
+  },
+  {
+    "revision": "ff8350c303be0296a9fb",
+    "url": "/sweet/static/js/main.788775a2.chunk.js"
+  },
+  {
+    "revision": "89b994b99d8bdbc060ab",
+    "url": "/sweet/static/js/runtime~main.df44ca87.js"
+  },
+  {
+    "revision": "8d6fa25da7a85cd59341",
+    "url": "/sweet/static/js/2.ce0aa7e2.chunk.js"
+  },
+  {
+    "revision": "261b3e104be35c4a24834fbbfb073abc",
+    "url": "/sweet/static/media/headshot.261b3e10.jpg"
+  },
+  {
+    "revision": "a3a413cefc0827bd1b89a345a2d2513f",
+    "url": "/sweet/index.html"
+  }
+];
